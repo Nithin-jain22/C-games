43e457f97970a80ecd60e09d5b413a701de2d67d
Nithin-jain22 <nithinjain000@gmail.com>
2025-05-21 10:45:09 +0530

    Update snake game.c

--- snake game.c
+++ snake game.c
@@ -1,118 +1,87 @@
 #include <stdio.h>
-#include <conio.h>
-#include <windows.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <time.h>
 
 #define WIDTH 20
-#define HEIGHT 20
-#define MAX_SNAKE 100
+#define HEIGHT 10
 
-int gameOver, score;
-int x, y, fruitX, fruitY;
-int tailX[MAX_SNAKE], tailY[MAX_SNAKE];
-int nTail;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-enum eDirection dir;
-
-void setup() {
-    gameOver = 0;
-    dir = STOP;
-    x = WIDTH / 2;
-    y = HEIGHT / 2;
-    fruitX = rand() % WIDTH;
-    fruitY = rand() % HEIGHT;
-    score = 0;
+int kbhit(void) {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+  
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+  
+    ch = getchar();
+  
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+  
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+  
+    return 0;
 }
 
-void draw() {
-    system("cls");
-
-    for (int i = 0; i < WIDTH + 2; i++) printf("#");
-    printf("\n");
-
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            if (j == 0) printf("#");
-            if (i == y && j == x)
-                printf("O"); // Snake head
-            else if (i == fruitY && j == fruitX)
-                printf("F"); // Fruit
-            else {
-                int print = 0;
-                for (int k = 0; k < nTail; k++) {
-                    if (tailX[k] == j && tailY[k] == i) {
-                        printf("o");
-                        print = 1;
-                    }
-                }
-                if (!print) printf(" ");
-            }
-            if (j == WIDTH - 1) printf("#");
+void draw(int x, int y, int fx, int fy) {
+    system("clear");
+    for (int i = 0; i <= HEIGHT; i++) {
+        for (int j = 0; j <= WIDTH; j++) {
+            if (i == 0 || i == HEIGHT || j == 0 || j == WIDTH)
+                printf("#");
+            else if (i == y && j == x)
+                printf("O");
+            else if (i == fy && j == fx)
+                printf("F");
+            else
+                printf(" ");
         }
         printf("\n");
-    }
-
-    for (int i = 0; i < WIDTH + 2; i++) printf("#");
-    printf("\nScore: %d\n", score);
-}
-
-void input() {
-    if (_kbhit()) {
-        switch (_getch()) {
-            case 'a': dir = LEFT; break;
-            case 'd': dir = RIGHT; break;
-            case 'w': dir = UP; break;
-            case 's': dir = DOWN; break;
-            case 'x': gameOver = 1; break;
-        }
-    }
-}
-
-void logic() {
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x;
-    tailY[0] = y;
-    for (int i = 1; i < nTail; i++) {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
-    }
-
-    switch (dir) {
-        case LEFT:  x--; break;
-        case RIGHT: x++; break;
-        case UP:    y--; break;
-        case DOWN:  y++; break;
-        default: break;
-    }
-
-    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
-        gameOver = 1;
-
-    for (int i = 0; i < nTail; i++)
-        if (tailX[i] == x && tailY[i] == y)
-            gameOver = 1;
-
-    if (x == fruitX && y == fruitY) {
-        score += 10;
-        fruitX = rand() % WIDTH;
-        fruitY = rand() % HEIGHT;
-        nTail++;
     }
 }
 
 int main() {
-    setup();
-    while (!gameOver) {
-        draw();
-        input();
-        logic();
-        Sleep(100); // adjust speed
+    int x = WIDTH / 2, y = HEIGHT / 2;
+    int fx = rand() % (WIDTH - 2) + 1;
+    int fy = rand() % (HEIGHT - 2) + 1;
+    int dx = 1, dy = 0;
+    char c;
+
+    while (1) {
+        draw(x, y, fx, fy);
+
+        if (kbhit()) {
+            c = getchar();
+            if (c == 'w') { dx = 0; dy = -1; }
+            if (c == 's') { dx = 0; dy = 1; }
+            if (c == 'a') { dx = -1; dy = 0; }
+            if (c == 'd') { dx = 1; dy = 0; }
+            if (c == 'q') break;
+        }
+
+        x += dx;
+        y += dy;
+
+        if (x <= 0 || x >= WIDTH || y <= 0 || y >= HEIGHT) break;
+
+        if (x == fx && y == fy) {
+            fx = rand() % (WIDTH - 2) + 1;
+            fy = rand() % (HEIGHT - 2) + 1;
+        }
+
+        usleep(200000);
     }
-    printf("Game Over! Final Score: %d\n", score);
+
+    printf("Game Over!\n");
     return 0;
 }
